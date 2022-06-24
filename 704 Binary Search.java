c68916295faf4569506a9e668ae38276e228e2f2
Ranjan Kumar <91800094+ranjankumar1602@users.noreply.github.com>
2022-06-24 11:37:10 +0530

    Update 704 Binary Search.java

--- 704 Binary Search.java
+++ 704 Binary Search.java
@@ -14,3 +14,36 @@ class Solution {
         return -1;
     }
 }
+
+
+/*
+Solution using recursion
+
+class Solution {
+    
+    private static int search(int[] arr,int start,int end,int target) {
+        if(start>end) {
+            return -1;
+        }
+        
+        int mid=(start + end)/2;
+        
+        if(arr[mid]==target) {
+            return mid;
+        }else if(target<arr[mid]) {
+            return search(arr,start,mid-1,target);
+        }else{
+            return search(arr,mid+1,end,target);
+        }
+        
+    }
+    
+    
+    public int search(int[] nums, int target) {
+        int end=nums.length-1;
+        int start=0;
+        
+        return search(nums,start,end,target);
+    }
+}
+*/
