a50b047cf6d7c407aa21d845942cb3f67394725c
Ranjan Kumar <91800094+ranjankumar1602@users.noreply.github.com>
2022-07-04 12:28:58 +0530

    Create 7 Reverse Integer.java

---
+++ 7 Reverse Integer.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int reverse(int x) {
+        long ans= 0;
+        while(x!=0) {
+            ans=(ans*10) + (x%10);
+            if(ans >Integer.MAX_VALUE) {
+                return 0;
+            }
+            if(ans < Integer.MIN_VALUE) {
+                return 0;
+            }
+            x /= 10;
+        }
+        
+        return (int)ans;
+    }
+}
