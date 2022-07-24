f1c36ead3cd19cad03a36f0dcb3f73792e6756af
Ranjan Kumar <91800094+ranjankumar1602@users.noreply.github.com>
2022-07-24 12:41:00 +0530

    Create 189 Rotate Array.java

---
+++ 189 Rotate Array.java
@@ -0,0 +1,21 @@
+class Solution {
+    
+    public void rotate(int[] nums, int low, int high) {
+        while(low<high) {
+            int temp=nums[low];
+            nums[low]=nums[high];
+            nums[high]=temp;
+            low++;
+            high--;
+        }
+    }
+    
+    
+    public void rotate(int[] nums, int k) {
+        int len=nums.length;
+        k = k % len;
+        rotate(nums, 0, len-k-1);
+        rotate(nums, len-k, len-1);
+        rotate(nums, 0, len-1);
+    }
+}
