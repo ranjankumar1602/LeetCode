3af279b33e50e09813a12e6389999f12c74af4a1
Ranjan Kumar <91800094+ranjankumar1602@users.noreply.github.com>
2023-03-17 20:11:47 +0530

    Update 1512 Number of Good Pairs.java

--- 1512 Number of Good Pairs.java
+++ 1512 Number of Good Pairs.java
@@ -1,5 +1,20 @@
 class Solution {
     public int numIdenticalPairs(int[] nums) {
+        int count = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i : nums) {
+            int temp = map.getOrDefault(i, 0);
+            count += temp;
+            map.put(i, temp + 1);
+        }
+        return count;
+    }
+}
+
+
+/*
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
         int count=0;
         for(int i=0; i<nums.length-1; i++) {
             for(int j=i+1; j<nums.length; j++) {
@@ -11,3 +26,4 @@ class Solution {
         return count;
     }
 }
+*/
