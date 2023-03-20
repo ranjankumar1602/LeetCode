31a77428142be7f832c81cdcc00fbeb8ffdc1489
Ranjan Kumar <91800094+ranjankumar1602@users.noreply.github.com>
2023-03-20 18:46:20 +0530

    Update 2395 Find Subarrays With Equal Sum.java

--- 2395 Find Subarrays With Equal Sum.java
+++ 2395 Find Subarrays With Equal Sum.java
@@ -1,16 +1,13 @@
 class Solution {
     public boolean findSubarrays(int[] nums) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=1;i<nums.length;i++){
-            int sum=nums[i]+nums[i-1];
-            map.put(sum,map.getOrDefault(sum,0)+1);         
-          }
-        for(Map.Entry<Integer, Integer> i:map.entrySet()){
-            if(i.getValue()>=2)
-               return true;
-            
-        } 
-        return false; 
-        
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 1; i < nums.length; i++) {
+            int sum = nums[i - 1] + nums[i];
+            if(set.contains(sum)) {
+                return true;
+            }
+            set.add(sum);
+        }
+        return false;
     }
 }
