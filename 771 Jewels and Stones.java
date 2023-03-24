20bed6fbc002c9347fd5aa7c91339d5f00d5ce32
Ranjan Kumar <91800094+ranjankumar1602@users.noreply.github.com>
2023-03-24 10:14:05 +0530

    Create 771 Jewels and Stones.java

---
+++ 771 Jewels and Stones.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char c : stones.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        int count = 0;
+        for(char c: jewels.toCharArray()) {
+            if(map.containsKey(c))
+                count += map.get(c);
+        }
+        return count;
+    }
+}
