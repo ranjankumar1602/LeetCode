a2eb03d22c8ed4ce39309136107a1e593da6e68b
Ranjan Kumar <91800094+ranjankumar1602@users.noreply.github.com>
2023-07-13 22:34:51 +0530

    Create 133 Clone Graph.java

---
+++ 133 Clone Graph.java
@@ -0,0 +1,40 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    private Node cloneGraph(Node node, Map<Integer, Node> map) {
+        if(map.containsKey(node.val)) {
+            return map.get(node.val);
+        }
+        Node clone = new Node(node.val);
+        map.put(node.val, clone);
+        for(Node neighbor : node.neighbors) {
+            clone.neighbors.add(cloneGraph(neighbor, map));
+        }
+        return clone;
+    }
+    public Node cloneGraph(Node node) {
+        if(node == null) {
+            return null;
+        }
+        Map<Integer, Node> map = new HashMap<>();
+        return cloneGraph(node, map);
+    }
+}
