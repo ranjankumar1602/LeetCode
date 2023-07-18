2710a5c9cea194de2aa3fae8189a54055b1b5799
Ranjan Kumar <91800094+ranjankumar1602@users.noreply.github.com>
2023-07-18 19:47:59 +0530

    Create 1261 Find Elements in a Contaminated Binary Tree.java

---
+++ 1261 Find Elements in a Contaminated Binary Tree.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class FindElements {
+    private Set<Integer> visited = new HashSet<>();
+
+    private void FindElements(TreeNode root, int data) {
+        if(root == null) {
+            return;
+        }
+        visited.add(data);
+        root.val = data;
+        FindElements(root.left, 2 * data + 1);
+        FindElements(root.right, 2 * data + 2);
+    }
+
+    public FindElements(TreeNode root) {
+        FindElements(root, 0);
+    }
+    
+    public boolean find(int target) {
+        return visited.contains(target);
+    }
+}
+
+/**
+ * Your FindElements object will be instantiated and called as such:
+ * FindElements obj = new FindElements(root);
+ * boolean param_1 = obj.find(target);
+ */
