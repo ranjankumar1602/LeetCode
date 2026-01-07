29355642ce724c5ba4d7c53c9094e56d4e792e24
Ranjan Kumar <91800094+ranjankumar1602@users.noreply.github.com>
2026-01-07 20:30:24 +0530

    Create 1339 Maximum Product of Splitted Binary Tree.java

---
+++ 1339 Maximum Product of Splitted Binary Tree.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    long ans = 0;
+    long totalSum = 0;
+
+    private int dfs(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        int currSum = dfs(root.left) + dfs(root.right) + root.val;
+        ans = Math.max(ans, (totalSum - currSum) * currSum);
+
+        return currSum;
+    }
+    
+    public int maxProduct(TreeNode root) {
+        totalSum = dfs(root);
+
+        dfs(root);
+        return (int)(ans % (1e9+7));
+    }
+}
